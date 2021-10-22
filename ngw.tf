a3078826e469b145ffcca647e012faa96790a98d
Christopher Basketfield <chris.basketfield@gmail.com>
2021-10-22 15:33:36 -0800

    nat gateway generation

--- ngw.tf
+++ ngw.tf
@@ -1,12 +1,25 @@
 resource "aws_nat_gateway" "ngw" {
   for_each = toset(local.availability_zones)
 
-  # allocation_id     = var.nat_gateways.allocation_id
-  subnet_id = aws_subnet.ngw_subnet[each.key].id
+  allocation_id = aws_eip.ngw_eip[each.key].id
+  subnet_id     = aws_subnet.ngw_subnet[each.key].id
 
   tags = {
     "Availability Zone"    = each.key
     "Managed By Terraform" = "true"
     "Name"                 = "${var.name}-nat-gateway-${each.key}"
   }
+}
+
+resource "aws_eip" "ngw_eip" {
+  for_each = toset(local.availability_zones)
+
+  vpc = true
+
+  tags = {
+    "Managed By Terraform" = "true"
+    "Name"                 = "${var.name}-nat-gateway-${each.key}"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
 }
