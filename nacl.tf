19057c929487759c91df96d319ac7215856c6ea2
Christopher Basketfield <chris.basketfield@gmail.com>
2021-10-22 16:18:43 -0800

    nacls for argument subnet groups

---
+++ nacl.tf
@@ -0,0 +1,12 @@
+resource "aws_network_acl" "nacl" {
+  for_each = { for group in var.subnet_groups : group.name => group }
+
+  subnet_ids = [for az in each.value.availability_zones : aws_subnet.subnet["${var.name}-${each.value.name}-${az}"].id]
+  vpc_id     = aws_vpc.vpc.id
+
+  tags = merge(each.value.tags, {
+    "Managed By Terraform" = "true"
+    "Name"                 = each.value.name
+    "Type"                 = each.value.type
+  })
+}
