20c6a29e19b7bfe1e217214dccb85488a43d83d7
Christopher Basketfield <chris.basketfield@gmail.com>
2021-10-25 17:26:58 -0800

    oof

--- dhcp-options.tf
+++ dhcp-options.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_dhcp_options" "dhcp_options" {
   domain_name = coalesce(
     var.dhcp.domain_name,
-    data.aws_region.current.name == "us-east-1" ? "ec2.internal" : "${var.region}.compute.amazonaws.com"
+    data.aws_region.current.name == "us-east-1" ? "ec2.internal" : "${data.aws_region.current.name}.compute.amazonaws.com"
   )
 
   domain_name_servers = coalesce(
