c0e886208794657a876e871ee7eb64f1a07eaaa6
Christopher Basketfield <chris.basketfield@gmail.com>
2024-03-05 13:31:01 -0900

    aws 5.0

--- _versions.tf
+++ _versions.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>3.6"
+      version = ">= 5.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "~>4.0"
+      version = ">= 4.0"
     }
   }
 }
